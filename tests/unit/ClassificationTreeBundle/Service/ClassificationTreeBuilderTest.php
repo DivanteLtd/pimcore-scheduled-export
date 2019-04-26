45a8dbc054f2e21e1d6d42d058e2af8e79874aee
Bruno Ramalho <bramalho@divante.pl>
2019-04-26 13:32:38 +0200

    init tests

---
+++ tests/unit/ClassificationTreeBundle/Service/ClassificationTreeBuilderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit\Divante\ScheduledExportBundle\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class ClassificationTreeBuilderTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
